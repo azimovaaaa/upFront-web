fcd211ded5ef25ee9aadb3f76fa32ecfbad6b7f8
Noah Czelusta <swimninja247@gmail.com>
2023-02-17 12:55:24 -0500

    Removed extra routes from mailing list site

--- src/App.js
+++ src/App.js
@@ -18,15 +18,7 @@ function App() {
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/home-investor' exact component={Home_investor} />
-          <Route path='/login-landlord' component={Login_landlord} />
-          <Route path='/login-investor' component={Login_investor} />
-          <Route path='/faq' component={Faq} />
-          <Route path='/products' component={Products} />
           <Route path='/learn-more' component={LearnMoreContainer} />
-          <Route path='/landlord-connect' component={Landlord} />
-          <Route path='/landlord-marketplace' component={Lmarketplace} />
-          <Route path='/landlord-dashboard' component={Ldashboard} />
-          <Route path='/sign-up' component={SignUp} />
         </Switch>
       </Router>
     </>
