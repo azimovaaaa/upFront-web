f265f795409a44c3094b373ca145cd715626020b
azimovaaaa <timazimova@gmail.com>
2023-04-05 13:36:15 -0400

    completed the upload design

--- src/components/Manual.js
+++ src/components/Manual.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Importer, ImporterField } from 'react-csv-importer';
 import axios from 'axios';
 import 'react-csv-importer/dist/index.css';
-
+import { Button } from './Button';
 import '../App.css';
 import './Manual.css';
 import {Footer2} from './Footer';
@@ -73,20 +73,109 @@ const Manual = () => {
     return (
         <>
         <Navbar2 />
-        <Importer
-            dataHandler={dataHandler}
-            chunkSize={10000} // optional, internal parsing chunk size in bytes
-            defaultNoHeader={false} // optional, keeps "data has headers" checkbox off by default
-            restartable={true} // optional, lets user choose to upload another file when import is complete
-            onStart={onStart}
-            onComplete={onComplete}
-            onClose={onClose}
-        >
-            <ImporterField name="address" label="Address" />
-            <ImporterField name="unitType" label="Unit Type" />
-            <ImporterField name="occupancyStatus" label="Occupancy Status" />
-            <ImporterField name="grossValue" label="Gross Value" optional />
-        </Importer>
+        <div className='manual-container'>
+            <Importer
+                dataHandler={dataHandler}
+                chunkSize={10000} // optional, internal parsing chunk size in bytes
+                defaultNoHeader={false} // optional, keeps "data has headers" checkbox off by default
+                restartable={true} // optional, lets user choose to upload another file when import is complete
+                onStart={onStart}
+                onComplete={onComplete}
+                onClose={onClose}
+            >
+                <ImporterField name="address" label="Address" />
+                <ImporterField name="unitType" label="Unit Type" />
+                <ImporterField name="occupancyStatus" label="Occupancy Status" />
+                <ImporterField name="grossValue" label="Gross Value" optional />
+            </Importer>
+            <div className='heading'>
+                <div className='manual-heading'>
+                    <h1>File Format Requirements</h1>
+                </div>
+                <div className='template-bttn'>
+                        <Button>
+                            Download Template
+                        </Button>
+                </div>
+            </div>
+
+            <div className='manual-table'>
+                <table className='upload-table'>
+                <thead>
+                <tr>
+                    <th >Column</th>
+                    <th >Format</th>
+                    <th >Example</th>
+                    <th >Notes</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>plan_amount</td>
+                        <td>Money</td>
+                        <td>12</td>
+                        <td>The amount in USD the customer is charged</td>
+                    </tr>
+                    <tr>
+                        <td>plan_amount</td>
+                        <td>number</td>
+                        <td>$99.00</td>
+                        <td>Name of the plan the subscription is on</td>
+                    </tr>
+                    <tr>
+                        <td>plan_amount</td>
+                        <td>String</td>
+                        <td>Sub-1</td>
+                        <td>End date of subscription, can be blank</td>
+                    </tr>
+                    <tr>
+                        <td>plan_amount</td>
+                        <td>Date: m/d/y</td>
+                        <td>10/1/2020</td>
+                        <td>Start date of subscription</td>
+                    </tr>
+                    <tr>
+                        <td>plan_amount</td>
+                        <td>Date: m/d/y</td>
+                        <td>Jane Doe</td>
+                        <td>Unique identifier for each subscription</td>
+                    </tr>
+                    <tr>
+                        <td>plan_amount</td>
+                        <td>Money</td>
+                        <td>$99.00</td>
+                        <td>The amount in USD the customer is charged</td>
+                    </tr>
+                    <tr>
+                        <td>plan_amount</td>
+                        <td>number</td>
+                        <td>$99.00</td>
+                        <td>Name of the plan the subscription is on</td>
+                    </tr>
+                    <tr>
+                        <td>plan_amount</td>
+                        <td>String</td>
+                        <td>Sub-1</td>
+                        <td>End date of subscription, can be blank</td>
+                    </tr>
+                    <tr>
+                        <td>plan_amount</td>
+                        <td>Date: m/d/y</td>
+                        <td>10/1/2020</td>
+                        <td>Start date of subscription</td>
+                    </tr>
+                    <tr>
+                        <td>plan_amount</td>
+                        <td>Date: m/d/y</td>
+                        <td>Jane Doe</td>
+                        <td>Unique identifier for each subscription</td>
+                    </tr>
+
+                </tbody>
+
+                </table>
+            </div>
+        </div>
         <Footer2 />
         </>
     );
