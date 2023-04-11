4f846fdd9245efaa4ce5bb866570c662d72c3485
azimovaaaa <72625921+azimovaaaa@users.noreply.github.com>
2023-04-11 16:09:13 -0400

    Upload (#3)
    * Added CSV importer
    
    * Functioning uplaod
    
    * completed the upload design
    
    * Updated cols
    
    * Updated examples table
    
    ---------
    
    Co-authored-by: swimninja247 <swimninja247@gmail.com>

--- src/components/Manual.js
+++ src/components/Manual.js
@@ -1,331 +1,147 @@
 import React, { useState } from 'react';
+import { Importer, ImporterField } from 'react-csv-importer';
+import axios from 'axios';
+import 'react-csv-importer/dist/index.css';
+import { Button } from './Button';
 import '../App.css';
 import './Manual.css';
 import {Footer2} from './Footer';
 import {Navbar2} from './Navbar';
-import { Button } from './Button';
+import { parseAddress } from '../utils/Address';
 
-var upload = document.getElementById('upload');
 
-function onFile() {
-    var me = this,
-        file = upload.files[0],
-        name = file.name.replace(/.[^/.]+$/, '');
-    console.log('upload code goes here', file, name);
-
-    upload.addEventListener('dragenter', function (e) {
-        upload.parentNode.className = 'area dragging';
-    }, false);
-
-    upload.addEventListener('dragleave', function (e) {
-        upload.parentNode.className = 'area';
-    }, false);
-
-    upload.addEventListener('dragdrop', function (e) {
-        onFile();
-    }, false);
-
-    upload.addEventListener('change', function (e) {
-        onFile();
-    }, false);
+const sendPropertyData = (payload) => {
+    console.log(payload)
+    return axios({
+        method: 'post',
+        url: 'http://localhost:8000/landlord/api/properties',
+        data : payload
+    })
+        .then((response) => {
+            console.log(response);
+            return response.data;
+        })
+        .catch((error) => {
+            console.error('Error sending property data:', error);
+            return null;
+        });
 }
 
+const dataHandler = async (rows, { startIndex }) => {
+    // required, may be called several times
+    // receives a list of parsed objects based on defined fields and user column mapping;
+    // (if this callback returns a promise, the widget will wait for it before parsing more data)
+    console.log("received batch of rows", rows);
 
-function Manual() {
-    const [isOpen, setIsOpen] = useState(false);
+    for (const property of rows) {
+        const addressString = property.address;
+        const parsedAddress = parseAddress(addressString);
 
-    const togglePopup = () => {
-        setIsOpen(!isOpen);
+        let payload = { 
+            landlord_id: 1,
+            address: parsedAddress,
+            property: {
+                unit_type: property.unitType,
+                occupied: true,
+                gross_value: property.rentAmount * property.contractLength,
+                upfront_capital: 0,
+                investor_return: 0,
+            }
+        };
+
+        sendPropertyData(payload);
     }
+} 
 
-  return (
-    <>
-    <Navbar2 />
-    <div className='manual-container'>
-        <div className='manual-heading'> 
-            <h1>Upload CSV file</h1>
-        </div>
-        <div class="area">
-            <input type="file" id="upload" placeholder='Click or drag to upload template with your customers filled in' />
-        </div>
-
-        <a class="button" href="#popup_flight_travlDil1">Click Me</a> 
-        <a class="button" href="#popup_flight_travlDil2">Click Me</a>
-        <a class="button" href="#popup_flight_travlDil3">Click Me</a>
-
-        <div className='heading'>
-            <div className='manual-heading'>
-                <h1>File Format Requirements</h1>
-            </div>
-            <div className='template-bttn'>
-                    <Button>
-                        Download Template
-                    </Button>
-            </div>
-        </div>
-        
-        <div className='manual-table'>
-            <table className='upload-table'>
-            <thead>
-            <tr>
-                <th >Column</th>
-                <th >Format</th>
-                <th >Example</th>
-                <th >Notes</th>
-            </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>plan_amount</td>
-                    <td>Money</td>
-                    <td>12</td>
-                    <td>The amount in USD the customer is charged</td>
-                </tr>
-                <tr>
-                    <td>plan_amount</td>
-                    <td>number</td>
-                    <td>$99.00</td>
-                    <td>Name of the plan the subscription is on</td>
-                </tr>
-                <tr>
-                    <td>plan_amount</td>
-                    <td>String</td>
-                    <td>Sub-1</td>
-                    <td>End date of subscription, can be blank</td>
-                </tr>
-                <tr>
-                    <td>plan_amount</td>
-                    <td>Date: m/d/y</td>
-                    <td>10/1/2020</td>
-                    <td>Start date of subscription</td>
-                </tr>
-                <tr>
-                    <td>plan_amount</td>
-                    <td>Date: m/d/y</td>
-                    <td>Jane Doe</td>
-                    <td>Unique identifier for each subscription</td>
-                </tr>
-                <tr>
-                    <td>plan_amount</td>
-                    <td>Money</td>
-                    <td>$99.00</td>
-                    <td>The amount in USD the customer is charged</td>
-                </tr>
-                <tr>
-                    <td>plan_amount</td>
-                    <td>number</td>
-                    <td>$99.00</td>
-                    <td>Name of the plan the subscription is on</td>
-                </tr>
-                <tr>
-                    <td>plan_amount</td>
-                    <td>String</td>
-                    <td>Sub-1</td>
-                    <td>End date of subscription, can be blank</td>
-                </tr>
-                <tr>
-                    <td>plan_amount</td>
-                    <td>Date: m/d/y</td>
-                    <td>10/1/2020</td>
-                    <td>Start date of subscription</td>
-                </tr>
-                <tr>
-                    <td>plan_amount</td>
-                    <td>Date: m/d/y</td>
-                    <td>Jane Doe</td>
-                    <td>Unique identifier for each subscription</td>
-                </tr>
-
-            </tbody>
-            </table>
-
-        </div>
-
-        
-
-    <div id="popup_flight_travlDil1" class="overlay_flight_traveldil">
-        <div class="popup_flight_travlDil">
-            <a class="close_flight_travelDl" href="#">&times;</a>
-            <div class="content_flightht_travel_dil">
-                <b>Based on your upload, we believe that your file has the following information:</b>
-                <table>
-                    <thead>
-                    <tr>
-                        <th></th>
-                        <th><b>Your Information</b></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Address</td>
-                            <td>411 West 112th St, 191 Claremont Ave, 122 La Salle Ave, 566</td>
-                        </tr>
-                        <tr>
-                            <td>Tenant Name</td>
-                            <td>The Smiths, The Kims, Miller Family, DeCarlo Family, Garcias</td>
-                        </tr>
-                        <tr>
-                            <td>Location</td>
-                            <td>New York, NY</td>
-                        </tr>
-                        <tr>
-                            <td>Monthly Rent</td>
-                            <td>$1200/month, $1300/month, $2300/month, $1700/month, $18</td>
-                        </tr>
-                        <tr>
-                            <td>Size</td>
-                            <td>300 sq feet, 600 sq feet, 1800 sq feet, 900 sq feet, 1200 sq feet</td>
-                        </tr>
-                        <tr>
-                            <td>Gross Value</td>
-                            <td>$32000, $23000, $64000, $22000, $52000, $50000, $20000</td>
-                        </tr>
-                        <tr>
-                            <td>Term Remainder</td>
-                            <td>8 months, 14 months, 9 months, 10 months, 24 months, 15 months</td>
-                        </tr>
-                        <tr>
-                            <td>Occupancy Status</td>
-                            <td>Occupied</td>
-                        </tr>
-                        <tr>
-                            <td>Email</td>
-                            <td>-</td>
-                        </tr>
-                        <tr>
-                            <td>Contact Type</td>
-                            <td>-</td>
-                        </tr>
-                        <tr>
-                            <td>Lease length</td>
-                            <td>-</td>
-                        </tr>
-                    </tbody>
-                </table>
-                <div className='heading'>
-                    <div className='manual-heading'>
-                        <b>Please confirm that this is accurate, by clicking Confirm. Otherwise, click Make Changes.</b>
-                    </div>
-                    <div className='template-bttn'>
-                            <Button>
-                                Make Changes
-                            </Button>
-                        
-                            <Button>
-                                Confirm
-                            </Button>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
-
-
-    <div id="popup_flight_travlDil2" class="overlay_flight_traveldil">
-        <div class="popup_flight_travlDil">
-            <a class="close_flight_travelDl" href="#">&times;</a>
-            <div class="content_flightht_travel_dil">
-                <b>You clicked Make Changes, select all of the titles that you’d like to change:</b>
-                <table>
-                    <thead>
-                    <tr>
-                        <th></th>
-                        <th><b>Your Information</b></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td><input type="checkbox"/></td>
-                            <td>Address</td>
-                            <td>411 West 112th St, 191 Claremont Ave, 122 La Salle Ave, 566</td>
-                        </tr>
-                        <tr>
-                            <td><input type="checkbox"/></td>
-                            <td>Tenant Name</td>
-                            <td>The Smiths, The Kims, Miller Family, DeCarlo Family, Garcias</td>
-                        </tr>
-                        <tr>
-                            <td><input type="checkbox"/></td>
-                            <td>Location</td>
-                            <td>New York, NY</td>
-                        </tr>
-                        <tr>
-                            <td><input type="checkbox"/></td>
-                            <td>Monthly Rent</td>
-                            <td>$1200/month, $1300/month, $2300/month, $1700/month, $18</td>
-                        </tr>
-                        <tr>
-                            <td><input type="checkbox"/></td>
-                            <td>Size</td>
-                            <td>300 sq feet, 600 sq feet, 1800 sq feet, 900 sq feet, 1200 sq feet</td>
-                        </tr>
-                        <tr>
-                            <td><input type="checkbox"/></td>
-                            <td>Gross Value</td>
-                            <td>$32000, $23000, $64000, $22000, $52000, $50000, $20000</td>
-                        </tr>
-                        <tr>
-                            <td><input type="checkbox"/></td>
-                            <td>Term Remainder</td>
-                            <td>8 months, 14 months, 9 months, 10 months, 24 months, 15 months</td>
-                        </tr>
-                        <tr>
-                            <td><input type="checkbox"/></td>
-                            <td>Occupancy Status</td>
-                            <td>Occupied</td>
-                        </tr>
-                        <tr>
-                            <td><input type="checkbox"/></td>
-                            <td>Email</td>
-                            <td>-</td>
-                        </tr>
-                        <tr>
-                            <td><input type="checkbox"/></td>
-                            <td>Contact Type</td>
-                            <td>-</td>
-                        </tr>
-                        <tr>
-                            <td><input type="checkbox"/></td>
-                            <td>Lease length</td>
-                            <td>-</td>
-                        </tr>
-                    </tbody>
-                </table>
-                <div className='heading'>
-                    <div className='manual-heading'>
-                        <b>Click Next to add/edit selected titles</b>
-                    </div>
-                    <div className='template-bttn'>
-                            <Button>
-                                Next
-                            </Button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <div id="popup_flight_travlDil3" class="overlay_flight_traveldil">
-        <div class="popup_flight_travlDil">
-            <a class="close_flight_travelDl" href="#">&times;</a>
-            <div class="content_flightht_travel_dil">
-                <b>Based on your upload, we believe that your file has the following information:</b>
-                
-            </div>
-        </div>
-    </div>
-    </div>
-    <Footer2 />
-    </>
-    
-
-  );
-  
+const onStart = ({ file, fields }) => {
+    // optional, invoked when user has mapped columns and started import
+    console.log("starting import of file", file, "with fields", fields);
 }
 
+const onComplete = ({ file, fields }) => {
+    // optional, invoked right after import is done (but user did not dismiss/reset the widget yet)
+    console.log("finished import of file", file, "with fields", fields);
+}
 
+const onClose = () => {
+    // optional, invoked when import is done and user clicked "Finish"
+    // (if this is not specified, the widget lets the user upload another file)
+    console.log("importer dismissed");
+}
+
+const Manual = () => {
+    return (
+        <>
+        <Navbar2 />
+        <div className='manual-container'>
+            <Importer
+                dataHandler={dataHandler}
+                chunkSize={10000} // optional, internal parsing chunk size in bytes
+                defaultNoHeader={false} // optional, keeps "data has headers" checkbox off by default
+                restartable={true} // optional, lets user choose to upload another file when import is complete
+                onStart={onStart}
+                onComplete={onComplete}
+                onClose={onClose}
+            >
+                <ImporterField name="address" label="Address" />
+                <ImporterField name="unitType" label="Unit Type" />
+                <ImporterField name="occupancyStatus" label="Occupancy Status" />
+                <ImporterField name="rentAmout" label="Rent Amount" />
+                <ImporterField name="contractLength" label="Contract Length" />
+            </Importer>
+            <div className='heading'>
+                <div className='manual-heading'>
+                    <h1>File Format Requirements</h1>
+                </div>
+                <div className='template-bttn'>
+                        <Button>
+                            Download Template
+                        </Button>
+                </div>
+            </div>
+
+            <div className='manual-table'>
+                <table className='upload-table'>
+                    <thead>
+                        <tr>
+                            <th >Column</th>
+                            <th >Format</th>
+                            <th >Example</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Address</td>
+                            <td>String</td>
+                            <td>123 Abc Rd Unit 4 New York, NY 10001</td>
+                        </tr>
+                        <tr>
+                            <td>Unit Type</td>
+                            <td>String</td>
+                            <td>3 BR Apartment</td>
+                        </tr>
+                        <tr>
+                            <td>Occupancy Status</td>
+                            <td>True/False</td>
+                            <td>TRUE</td>
+                        </tr>
+                        <tr>
+                            <td>Rent Amount</td>
+                            <td>Number</td>
+                            <td>1234.56</td>
+                        </tr>
+                        <tr>
+                            <td>Contract Length (in months)</td>
+                            <td>Number</td>
+                            <td>3</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <Footer2 />
+        </>
+    );
+}
     
-
 export default Manual;
