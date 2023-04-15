07aaf0d0ebbf75fdc60d1eef049fe8b65c6486b7
swimninja247 <swimninja247@gmail.com>
2023-04-15 18:40:29 -0400

    Fixed another merge conflict

--- src/components/Manual.js
+++ src/components/Manual.js
@@ -1,65 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import { Importer, ImporterField } from 'react-csv-importer';
-import axios from 'axios';
-import 'react-csv-importer/dist/index.css';
-import { Button } from './Button';
-import '../App.css';
-import './Manual.css';
-import {Footer2} from './Footer';
-import {Navbar2} from './Navbar';
-import { parseAddress } from '../utils/Address';
-
-
-const sendPropertyData = (payload) => {
-    console.log(payload)
-    return axios({
-        method: 'post',
-        url: 'http://localhost:8000/landlord/api/properties',
-        data : payload
-    })
-        .then((response) => {
-            console.log(response);
-            return response.data;
-        })
-        .catch((error) => {
-            console.error('Error sending property data:', error);
-            return null;
-        });
-}
-
-const dataHandler = async (rows, { startIndex }) => {
-    // required, may be called several times
-    // receives a list of parsed objects based on defined fields and user column mapping;
-    // (if this callback returns a promise, the widget will wait for it before parsing more data)
-    console.log("received batch of rows", rows);
-
-    for (const property of rows) {
-        const addressString = property.address;
-        const parsedAddress = parseAddress(addressString);
-
-        let payload = { 
-            landlord_id: 1,
-            address: parsedAddress,
-            property: {
-                unit_type: property.unitType,
-                occupied: true,
-                gross_value: property.grossValue,
-                upfront_capital: 0,
-                investor_return: 0,
-            }
-        };
-
-        sendPropertyData(payload);
-    }
-} 
-
-const onStart = ({ file, fields }) => {
-    // optional, invoked when user has mapped columns and started import
-    console.log("starting import of file", file, "with fields", fields);
-}
-
-=======
 import React from 'react';
 import { Importer, ImporterField } from 'react-csv-importer';
 import 'react-csv-importer/dist/index.css';
@@ -98,12 +36,10 @@ const onStart = ({ file, fields }) => {
     console.log("starting import of file", file, "with fields", fields);
 }
 
->>>>>>> master
 const onComplete = ({ file, fields }) => {
     // optional, invoked right after import is done (but user did not dismiss/reset the widget yet)
     console.log("finished import of file", file, "with fields", fields);
 }
-<<<<<<< HEAD
 
 const onClose = () => {
     // optional, invoked when import is done and user clicked "Finish"
@@ -111,15 +47,6 @@ const onClose = () => {
     console.log("importer dismissed");
 }
 
-=======
-
-const onClose = () => {
-    // optional, invoked when import is done and user clicked "Finish"
-    // (if this is not specified, the widget lets the user upload another file)
-    console.log("importer dismissed");
-}
-
->>>>>>> master
 const Manual = () => {
     return (
         <>
