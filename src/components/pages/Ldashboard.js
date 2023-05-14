d2554110754d63b520e59b17ac2b47c7361c5a11
swimninja247 <swimninja247@gmail.com>
2023-05-14 11:17:53 -0500

    Ldashboard values update on bid

--- src/components/pages/Ldashboard.js
+++ src/components/pages/Ldashboard.js
@@ -7,16 +7,20 @@ import { TableDash } from '../Ltable';
 import {DashCard} from '../Dashcard';
 import PropertyService from '../../services/PropertyService';
 import { countPosted, formatProperty } from '../../utils/Properties'
+import BidService from '../../services/BidService';
 
 const Ldashboard = () => {
 
   const [properties, setProperties] = useState([]);
+  const [existsBids, setExistsBids] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const data = await PropertyService.get();
         setProperties(data.map(property => formatProperty(property)));
+        const bids = await BidService.get()
+        setExistsBids(bids.length !== 0);
       } catch (error) {
         console.error('Error fetching property data:', error);
       }
@@ -29,11 +33,11 @@ const Ldashboard = () => {
       <Navbar2 />
       <DashCard 
         contractsOutstanding={properties.length} 
-        avgPercentReceived={properties.length !== 0 ? '42' : '0'} 
-        targetIRR={properties.length !== 0 ? '4.2' : '0'} 
+        avgPercentReceived={existsBids ? '42' : '0'} 
+        targetIRR={existsBids ? '4.2' : '0'} 
         contractsNotOnMarketplace={properties.length - countPosted(properties)} 
         totalContractsCompleted={countPosted(properties) / 2} 
-        ROI={properties.length !== 0 ? '4.2' : '0'} 
+        ROI={existsBids ? '4.2' : '0'} 
       />
       <TableDash data={properties}/>
       <Footer2 />
